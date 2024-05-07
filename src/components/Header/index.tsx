2252a58d1ef8bf96079e940b4562fb43b2e6eebd
Kauê Henrick <kauek78942@gmail.com>
2024-05-07 17:30:00 -0300

    07-05

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.scss';
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-            <img src="images/logo.svg" alt=""/>
+            <img src="images/logo.png" alt=""/>
                 <nav>
                     <a className={styles.active} href="">Home</a>
                     <a href="">Posts</a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,55 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { GetStaticProps } from "next";
 
-export default function Home() {
+import styles from './home.module.scss'
+import { stripe } from "../services/stripe";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | News</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <span>👏 Hey, welcome</span>
+            <h1>News about the <span>React</span> world.</h1>
+            <p>
+              Get acess to all the publications <br />
+              <span>For {product.amount} month</span>
+            </p>
+            <SubscribeButton priceId={product.priceId} />
+          </section>
+
+          <img src="/images/avatar.png" alt=""/>
+      </main>
     </>
   );
+}
+ 
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1PDm3lRpeutC65fBTp1tDt4i');
+
+  const product = {
+    priceID: price.id,
+    amount: new Intl.NumberFormat('en-us', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    }, 
+    revalidate: 60 * 60 * 24, //24 hours
+  }
 }
