9a8a791e2fc45fe78bba852ef8872d84805607f9
Kauê Henrick <kauek78942@gmail.com>
2024-05-10 17:32:02 -0300

    wtf

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna";
 
 export const authOptions = {
   providers: [
@@ -9,5 +13,21 @@ export const authOptions = {
       
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 }
 export default NextAuth(authOptions)
