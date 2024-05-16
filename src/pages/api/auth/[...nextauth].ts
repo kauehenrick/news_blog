a4d7eb80656caaa40f5d6cb2994b426857b15d6d
Kauê Henrick <kauek78942@gmail.com>
2024-05-16 11:44:04 -0300

    error

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,11 +19,26 @@ export const authOptions = {
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+              q.Create(
+                q.Collection('users'),
+                { data: { email } }
+              ),
+              q.Get(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
           )
         )
+
         return true;
       } catch {
         return false;
